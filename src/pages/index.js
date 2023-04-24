0625ac75c392fa2556c9569ad9cf1281abf9fe5f
bacnk <92347442+bacnk@users.noreply.github.com>
2023-04-24 07:09:35 +0700

    beginner
    - tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,5 @@
-import { useState } from 'react'
 
-export default function BinaryUpload() {
-  const [file, setFile] = useState(null)
-  const [message, setMessage] = useState('')
-
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0])
-  }
-
-  const handleUpload = async () => {
-    if (!file) {
-      setMessage('Please select a file to upload')
-      return
-    }
-
-    const formData = new FormData()
-    formData.append('binary', file)
-
-    const response = await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
-    })
-
-    const data = await response.json()
-    setMessage(data.message)
-  }
-
-  return (
-    <div>
-      <h1>Upload Binary File 111</h1>
-      <div>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={handleUpload}>Upload</button>
-      </div>
-      <p>{message}</p>
-    </div>
-  )
+function HomePage(){
+  return <div> Welcom next js</div>
 }
+export default HomePage;
